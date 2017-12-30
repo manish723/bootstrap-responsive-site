50f7e852f2ba96d8f9ceba0317fa1152d62d6bb6
manish723 <manishkumar723723@gmail.com>
2017-12-29 17:58:23 -0800

    Add files via upload

---
+++ includes/js/script.js
@@ -0,0 +1,20 @@
+$(function() {
+	
+	$('#alertMe').click(function(e) {
+		
+		e.preventDefault();
+		
+		$('#successAlert').slideDown();
+		
+	});
+	
+	$('a.pop').click(function(e) {
+		e.preventDefault();
+	});
+	
+	$('a.pop').popover();
+	
+	$('[rel="tooltip"]').tooltip();
+	
+	
+});
